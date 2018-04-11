d44dc444973ac318d50d0c0795a369d100118a24
inicolini <irene.nicolini@iks.is>
2018-04-11 11:27:23 +0200

    semi-fix routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {TestComponent} from './components/test.component';
-import {HomeComponent} from './components/home.component';
-import {FertilizationComponent} from './components/fertilization.component';
-import {LoginComponent} from './components/login.component';
-import {RegisterComponent} from './components/register.component';
+import { TestComponent } from './components/test.component';
+import { HomeComponent } from './components/home.component';
+import { FertilizationComponent } from './components/fertilization.component';
+import { LoginComponent } from './components/login.component';
+import { RegisterComponent } from './components/register.component';
 import { MaintenanceComponent } from './components/maintenance.component';
 
 const routes: Routes = [
@@ -15,11 +15,11 @@ const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full' },
-  { path: '*', redirectTo: '' }
+  { path: '**', redirectTo: '/home' }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
   exports: [RouterModule]
 })
 export class AppRoutingModule {}
