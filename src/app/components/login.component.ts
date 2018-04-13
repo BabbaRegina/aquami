f801f39f3565ad2df95ba01704a389b4d71360a0
inicolini <irene.nicolini@iks.is>
2018-04-13 15:58:58 +0200

    nuova icona, qualche fix

--- src/app/components/login.component.ts
+++ src/app/components/login.component.ts
@@ -8,28 +8,31 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
   templateUrl: './login.component.html'
 })
 export class LoginComponent implements OnInit {
-
   signupForm: FormGroup;
+  wrongPassword: boolean;
 
-  constructor(private authService: AuthenticationService, private router: Router) {}
+  constructor(
+    private authService: AuthenticationService,
+    private router: Router
+  ) {}
 
   ngOnInit() {
+    this.wrongPassword = false;
     this.signupForm = new FormGroup({
-      'password': new FormControl(null, Validators.required)
+      password: new FormControl(null, Validators.required)
     });
   }
 
   login() {
     console.log(this.signupForm);
-    if (this.signupForm.value.password) {
-      this.authService.login(this.signupForm.value.password);
-      this.authService.isAuthenticated().then((authenticated: boolean) => {
-        if (authenticated) {
-          this.router.navigate(['/home']);
-        }
-      });
-    } else {
-      console.log('password non valida');
-    }
+    this.authService.login(this.signupForm.value.password);
+    this.authService.isAuthenticated().then((authenticated: boolean) => {
+      if (authenticated) {
+        this.router.navigate(['/home']);
+      } else {
+        console.log('password non valida');
+        this.wrongPassword = true;
+      }
+    });
   }
 }
